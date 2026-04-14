7382d34cc02fba726dead7ba6923289032fbcc79
PRORAINDORP <santiagoclarencejames24@gmail.com>
2026-04-14 23:58:04 +0800

    update background image to bg.png and adjust rendering in PixelVelocity.py

--- PixelVelocity.py
+++ PixelVelocity.py
@@ -24,10 +24,12 @@ clock = pygame.time.Clock()
 
 pygame.display.set_caption("Pixel Velocity")
 
-image1= pygame.image.load("images/background.jpg")
+image1= pygame.image.load("images/bg.png")
 image2 = pygame.image.load("images/road.png")
 
+image1_1 = pygame.transform.smoothscale(image1, (1925, 1025))
 image2_1 = pygame.transform.smoothscale(image2, (1925, 450))
+image2_2 = pygame.transform.flip(image2_1, 0, 50)
 is_active = True
 
 while is_active:
@@ -35,8 +37,8 @@ while is_active:
         if event.type == pygame.QUIT:
             is_active = False
 
-    screen.blit(image1, (0, 0))
-    screen.blit(image2_1, (0, 500))
+    screen.blit(image1_1, (0, 0))
+    screen.blit(image2_2, (0, 600))
 
     pygame.display.flip()
     clock.tick(60)
